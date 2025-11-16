b8fbaec13c9bb2894599d88d67a1b6c7e74e3f6b
Gervig <gervig91@gmail.com>
2025-11-16 21:53:41 +0100

    Create binarysearch_recursive.js

---
+++ Binary Search/binarysearch_recursive.js
@@ -0,0 +1,57 @@
+export function binarySearchRecursive(
+  search,
+  values,
+  // default parameters, only set first time the function is called
+  start = 0,
+  end = values.length - 1,
+  iterations = 0
+) {
+
+  // start by counting iterations up by 1
+  iterations++;
+
+  // early exit condition
+  if (start > end) {
+    return {
+      found: false,
+      index: -1,
+      iterations,
+    };
+  }
+
+  // find the middle index, rounded down
+  let middleIndex = Math.floor((start + end) / 2);
+  // then we can find the middle value
+  let middleValue = values[middleIndex];
+
+  // check if the middle value is the search target
+  if (middleValue === search) {
+    // successful
+    // return object with properties
+    return {
+      found: true,
+      index: middleIndex + offSet,
+      iterations,
+    };
+  }
+  // else if the search target is in the upper end of values
+  else if (middleValue < search) {
+    return binarySearchRecursive(
+      search,
+      values,
+      middleIndex + 1, // new start
+      end,
+      iterations
+    );
+  }
+  // else it's in the lower end of values
+  else {
+    return binarySearchRecursive(
+      search,
+      values,
+      start,
+      middleIndex - 1, // new end
+      iterations
+    );
+  }
+}
