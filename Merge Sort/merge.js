f8f1d6184c79c574dc43255be23800c89bba1d4a
Casper Alexander Gervig <gervig91@gmail.com>
2025-11-20 15:51:37 +0100

    Update merge.js

--- Merge Sort/merge.js
+++ Merge Sort/merge.js
@@ -3,33 +3,31 @@ import { isSorted } from "../Sort/issorted.js";
 export function merge(leftObj, rightObj) {
   const left = leftObj.arr;
   const right = rightObj.arr;
-
   let arr = [];
   let i = 0,
     j = 0;
-
-  // accumulate iterations from children
+  // accumulate iterations from left and right
   let iterations = leftObj.iterations + rightObj.iterations;
 
   while (i < left.length && j < right.length) {
-    if (left[i] < right[j]) arr.push(left[i++]);
-    else arr.push(right[j++]);
+    // push left or right's lowest
+    if (left[i] < right[j]) {
+      arr.push(left[i++]);
+    } else {
+      arr.push(right[j++]);
+    }
     iterations++;
   }
 
+  // Add remaining elements
   while (i < left.length) {
     arr.push(left[i++]);
     iterations++;
   }
-
   while (j < right.length) {
     arr.push(right[j++]);
     iterations++;
   }
 
-  return {
-    arr: arr,
-    iterations: iterations,
-    sorted: isSorted(arr),
-  };
+  return { arr: arr, iterations: iterations, sorted: isSorted(arr) };
 }
