aaefd5f7c1b1a872e23db6c5f025fde306a0ec72
Gervig <gervig91@gmail.com>
2025-11-20 03:41:32 +0100

    mergesort wip

--- Merge Sort/mergesort.js
+++ Merge Sort/mergesort.js
@@ -1,4 +1,4 @@
-export function mergeSort(arrA, arrB) {
+export function mergeSortedArrays(arrA, arrB) {
   let arrC = [];
   let i = 0,
     j = 0;
@@ -11,9 +11,38 @@ export function mergeSort(arrA, arrB) {
     }
   }
 
-  // Append remaining elements
   while (i < arrA.length) arrC.push(arrA[i++]);
   while (j < arrB.length) arrC.push(arrB[j++]);
 
   return arrC;
+}
+
+export function mergeSort(arr) {
+  if (arr.length <= 1) return arr;
+
+  const mid = Math.floor(arr.length / 2);
+  const left = mergeSort(arr.slice(0, mid));
+  const right = mergeSort(arr.slice(mid));
+
+  return merge(left, right);
+}
+
+function merge(left, right) {
+  const result = [];
+  let i = 0,
+    j = 0;
+
+  while (i < left.length && j < right.length) {
+    if (left[i] < right[j]) {
+      result.push(left[i++]);
+    } else {
+      result.push(right[j++]);
+    }
+  }
+
+  // Add remaining elements
+  while (i < left.length) result.push(left[i++]);
+  while (j < right.length) result.push(right[j++]);
+
+  return result;
 }
--- Merge Sort/test_mergesort.js
+++ Merge Sort/test_mergesort.js
@@ -1,8 +1,23 @@
-import { mergeSort } from "./mergesort.js";
+import { mergeSortedArrays, mergeSort } from "./mergesort.js";
+import { shuffle } from "../Shuffle/shuffle.js";
+import { isSorted } from "../Sort/issorted.js";
 
-const arrA = [2, 3, 4, 5, 7];
-const arrB = [0, 1, 6, 8, 9];
+let arrA = [2, 3, 4, 5, 7];
+let arrB = [0, 1, 6, 8, 9];
 
-let arrC = mergeSort(arrA, arrB);
+let arrC = mergeSortedArrays(arrA, arrB);
 
-console.log(`${arrC}`);
+console.log(`Merged sorted arrays: ${arrC}`);
+
+arrA = shuffle(arrA);
+arrB = shuffle(arrB);
+
+console.log(
+  `Shuffled arrays; \n A: ${arrA}, Sorted: ${isSorted(
+    arrA
+  )}\n B: ${arrB}, Sorted: ${isSorted(arrB)}`
+);
+
+let arrD = mergeSort(arrA, arrB);
+
+console.log(`Merge sorted A and B: ${arrD}`);
